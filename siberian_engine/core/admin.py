232cc432ba1f3b83d606292a020c9b18b87e73ba
MaestroXXXVIII <urmanov.pro@gmail.com>
2024-02-05 20:33:24 +0800

    register model for admin panel

--- siberian_engine/core/admin.py
+++ siberian_engine/core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Operation, Order
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    prepopulated_fields = {'slug': ('title', )}
+
+
+@admin.register(Operation)
+class OperationAdmin(admin.ModelAdmin):
+    list_display = ['category', 'title', 'user', 'price']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ['code',]
+    filter_horizontal = ('operation',)
+    list_display = ['code',
+                    'customer_name',
+                    'customer_phone_number',
+                    'engine_title',
+                    'total_amount']
--- siberian_engine/core/models.py
+++ siberian_engine/core/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     title = models.CharField(max_length=50)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
@@ -18,6 +19,7 @@ class Operation(models.Model):
                              on_delete=models.SET_NULL,
                              null=True,
                              blank=True)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
@@ -25,40 +27,50 @@ class Operation(models.Model):
 
 class Order(models.Model):
     """Заказ - наряд"""
-    code = models.CharField(max_length=255)
-    customer_name = models.CharField(max_length=50)
-    customer_phone_number = models.CharField(max_length=12)
-    engine_title = models.CharField(max_length=50)
-    accept_date = models.DateTimeField(auto_now=True)
-    complete_date = models.DateTimeField()
-    operation = models.ManyToManyField(Operation, related_name='task')
-    complete_operation_date = models.DateTimeField()
-    count_of_cylinders = models.IntegerField(default=0)
-    count_of_valves = models.IntegerField(default=0)
-    total_amount = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        return self.code + ' ' + self.customer_name
+    @staticmethod
+    def generate_code():
+        last_order = Order.objects.order_by('accept_date').last()
+        return f"{int(last_order.code) + 1}"
 
     def sum_operation(self):
         cost_operation = 0
 
         if self.count_of_cylinders != 0:
-            operations = self.operation.objects.exclude(category__title='Блок')
+            operations = self.operation.filter(category__title='Блок')
             for operation in operations:
-                cost_operation = operation.price * self.count_of_cylinders
+                print(operation.price)
+                cost_operation += operation.price * self.count_of_cylinders
+
             return cost_operation
 
         if self.count_of_valves != 0:
-            operations = self.operation.objects.exclude(category__title='ГБЦ')
+            operations = (self.operation.objects.filter(category__title='ГБЦ'))
             for operation in operations:
                 cost_operation = operation.price * self.count_of_valves
+
             return cost_operation
 
-    def sum_total_amount(self):
-        operations = self.operation.objects.all()
-        total_amount = self.total_amount
-        for operation in operations:
-            operation.price += total_amount
+    code = models.CharField(max_length=255,
+                            db_index=True,
+                            default=generate_code,
+                            )
+    customer_name = models.CharField(max_length=50)
+    customer_phone_number = models.CharField(max_length=12)
+    engine_title = models.CharField(max_length=50)
+    accept_date = models.DateTimeField(auto_now=True)
+    complete_date = models.DateTimeField(blank=True, null=True)
+    operation = models.ManyToManyField(Operation, related_name='task')
+    count_of_cylinders = models.IntegerField(default=0)
+    count_of_valves = models.IntegerField(default=0)
+    total_amount = models.PositiveIntegerField(default=0)
 
-        return total_amount
+    class Meta:
+        ordering = ['-accept_date']
+
+    def __str__(self):
+        return self.code + ' ' + self.customer_name
+
+    def save(self, *args, **kwargs):
+        self.total_amount = self.sum_operation()
+        super().save(*args, **kwargs)
+
