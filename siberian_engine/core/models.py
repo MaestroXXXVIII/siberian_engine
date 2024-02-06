0d844d817b6ba05edee1db958ae84b9b1579dcd1
MaestroXXXVIII <urmanov.pro@gmail.com>
2024-02-06 19:24:00 +0800

    small improvements

--- siberian_engine/core/models.py
+++ siberian_engine/core/models.py
@@ -3,8 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField('Название', max_length=50)
     slug = models.SlugField()
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
 
     def __str__(self):
         return self.title
@@ -12,17 +16,25 @@ class Category(models.Model):
 
 class Operation(models.Model):
     """Технологическая операция"""
-    title = models.CharField(max_length=125)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.PositiveIntegerField()
+    title = models.CharField('Название', max_length=125)
+    category = models.ForeignKey(Category,
+                                 verbose_name='Категория',
+                                 on_delete=models.CASCADE)
+    price = models.PositiveIntegerField('Цена')
     user = models.ForeignKey(User,
+                             verbose_name='Мастер',
                              on_delete=models.SET_NULL,
                              null=True,
                              blank=True)
     slug = models.SlugField()
 
+    class Meta:
+        ordering = ['slug']
+        verbose_name = 'операция'
+        verbose_name_plural = 'операции'
+
     def __str__(self):
-        return self.title
+        return f'{self.title} ({self.user.username})'
 
 
 class Order(models.Model):
@@ -50,22 +62,31 @@ class Order(models.Model):
 
             return cost_operation
 
-    code = models.CharField(max_length=255,
+    code = models.CharField('Номер заказа',
+                            max_length=255,
                             db_index=True,
                             default=generate_code,
                             )
-    customer_name = models.CharField(max_length=50)
-    customer_phone_number = models.CharField(max_length=12)
-    engine_title = models.CharField(max_length=50)
-    accept_date = models.DateTimeField(auto_now=True)
-    complete_date = models.DateTimeField(blank=True, null=True)
-    operation = models.ManyToManyField(Operation, related_name='task')
-    count_of_cylinders = models.IntegerField(default=0)
-    count_of_valves = models.IntegerField(default=0)
-    total_amount = models.PositiveIntegerField(default=0)
+    customer_name = models.CharField('Имя заказчика', max_length=50)
+    customer_phone_number = models.CharField('Номер заказчика', max_length=12)
+    engine_title = models.CharField('Наименование двигателя', max_length=50)
+    accept_date = models.DateTimeField('Дата приёма', auto_now=True)
+    count_of_cylinders = models.IntegerField('Количество цилиндров', default=0)
+    count_of_valves = models.IntegerField('Количество клапанов', default=0)
+    operation = models.ManyToManyField(Operation,
+                                       verbose_name='Операция',
+                                       related_name='task'
+                                       )
+    total_amount = models.PositiveIntegerField('Итоговая сумма', default=0)
+    comments = models.TextField('Комментарии', blank=True, null=True)
+    complete_date = models.DateTimeField('Дата выполнения',
+                                         blank=True,
+                                         null=True)
 
     class Meta:
         ordering = ['-accept_date']
+        verbose_name = 'заказ'
+        verbose_name_plural = 'заказы'
 
     def __str__(self):
         return self.code + ' ' + self.customer_name
