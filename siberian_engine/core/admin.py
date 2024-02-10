97a8c1e63d256a3e46753be22679ae182ba0912d
MaestroXXXVIII <urmanov.pro@gmail.com>
2024-02-10 17:42:56 +0800

    changing the database schema and fix bugs

--- siberian_engine/core/admin.py
+++ siberian_engine/core/admin.py
@@ -1,26 +1,69 @@
 from django.contrib import admin
 
-from .models import Category, Operation, Order
+from .models import CarBrand, Engine, CategoryOperations, Operation, Order
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+@admin.register(CarBrand)
+class CarBrandAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_filter = ['title',]
+
+
+@admin.register(Engine)
+class EngineAdmin(admin.ModelAdmin):
+    list_display = ['title', 'engine_type']
+    search_fields = ['title', 'engine_type']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(CategoryOperations)
+class CategoryOperationsAdmin(admin.ModelAdmin):
     list_display = ['title']
     prepopulated_fields = {'slug': ('title', )}
 
 
 @admin.register(Operation)
 class OperationAdmin(admin.ModelAdmin):
-    list_display = ['category', 'title', 'user', 'price']
+    list_display = ['title', 'engine', 'category', 'price']
+    list_editable = ['price']
+    list_filter = ['title', 'engine']
+    search_fields = ['title']
     prepopulated_fields = {'slug': ('title',)}
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    readonly_fields = ['code',]
+    readonly_fields = ['id',]
     filter_horizontal = ('operation',)
-    list_display = ['code',
+    list_display = ['id',
                     'customer_name',
                     'customer_phone_number',
-                    'engine_title',
+                    'engine',
                     'total_amount']
+    list_editable = ['total_amount',]
+    list_filter = ['customer_name',]
+    list_max_show_all = 100
+    list_select_related = ['engine']
+    search_fields = ['id', 'customer_name']
+    date_hierarchy = "accept_date"
+
+    @staticmethod
+    def sum_operations(data_form):
+        cost_operations = 0
+        operations = data_form
+
+        for operation in operations:
+            cost_operations += operation.price
+
+        return cost_operations
+
+    @staticmethod
+    def set_id():
+        last_order = Order.objects.order_by('id').last()
+        order_number = last_order.id + 1
+        return order_number
+
+    def save_model(self, request, obj, form, change):
+        obj.total_amount = self.sum_operations(form.cleaned_data['operation'])
+        obj.id = self.set_id()
+        super().save_model(request, obj, form, change)
--- siberian_engine/core/models.py
+++ siberian_engine/core/models.py
@@ -1,14 +1,56 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
 
 
-class Category(models.Model):
-    title = models.CharField('Название', max_length=50)
+class CarBrand(models.Model):
+    title = models.CharField("Название", max_length=50)
+
+    class Meta:
+        verbose_name = 'брэнд автомобиля'
+        verbose_name_plural = 'брэнды автомобиля'
+
+    def __str__(self):
+        return self.title
+
+
+class Engine(models.Model):
+
+    TRUCK_ENGINE = 'TE'
+    CAR = 'C'
+    ENGINE_TYPE = {
+        (TRUCK_ENGINE, "Truck Engine"),
+        (CAR, "Car")
+    }
+    brand = models.ForeignKey(CarBrand, verbose_name="Брэнд",
+                              on_delete=models.CASCADE,
+                              blank=True,
+                              null=True
+                              )
+    title = models.CharField("Название", max_length=100)
+    count_of_cylinders = models.IntegerField("Количество цилиндров", default=0)
+    count_of_valves = models.IntegerField("Количество клапанов", default=0)
+    engine_type = models.CharField("Тип двигателя", choices=ENGINE_TYPE)
     slug = models.SlugField()
 
     class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'категории'
+        verbose_name = 'двигатель'
+        verbose_name_plural = 'двигатели'
+        indexes = [
+            models.Index(fields=['title']),
+            models.Index(fields=['engine_type']),
+        ]
+
+    def __str__(self):
+        return f'{self.brand} {self.title}'
+
+
+class CategoryOperations(models.Model):
+    title = models.CharField("Название", max_length=50)
+    slug = models.SlugField()
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
 
     def __str__(self):
         return self.title
@@ -16,82 +58,65 @@ class Category(models.Model):
 
 class Operation(models.Model):
     """Технологическая операция"""
-    title = models.CharField('Название', max_length=125)
-    category = models.ForeignKey(Category,
-                                 verbose_name='Категория',
-                                 on_delete=models.CASCADE)
-    price = models.PositiveIntegerField('Цена')
-    user = models.ForeignKey(User,
-                             verbose_name='Мастер',
-                             on_delete=models.SET_NULL,
-                             null=True,
-                             blank=True)
+
+    title = models.CharField("Название", max_length=125)
+    category = models.ForeignKey(CategoryOperations,
+                                 verbose_name="Категория",
+                                 on_delete=models.CASCADE
+                                 )
+    engine = models.ForeignKey(Engine,
+                               verbose_name="Двигатель",
+                               on_delete=models.CASCADE,
+                               null=True,
+                               blank=True
+                               )
+    price = models.PositiveIntegerField("Цена")
     slug = models.SlugField()
 
     class Meta:
-        ordering = ['slug']
-        verbose_name = 'операция'
-        verbose_name_plural = 'операции'
+        ordering = ["title"]
+        indexes = [
+            models.Index(fields=['title']),
+            models.Index(fields=['engine'])
+        ]
+        verbose_name = "операция"
+        verbose_name_plural = "операции"
 
     def __str__(self):
-        return f'{self.title} ({self.user.username})'
+        return f"{self.title} ({self.engine})"
 
 
 class Order(models.Model):
     """Заказ - наряд"""
-    @staticmethod
-    def generate_code():
-        last_order = Order.objects.order_by('accept_date').last()
-        return f"{int(last_order.code) + 1}"
 
-    def sum_operation(self):
-        cost_operation = 0
-
-        if self.count_of_cylinders != 0:
-            operations = self.operation.filter(category__title='Блок')
-            for operation in operations:
-                print(operation.price)
-                cost_operation += operation.price * self.count_of_cylinders
-
-            return cost_operation
-
-        if self.count_of_valves != 0:
-            operations = (self.operation.objects.filter(category__title='ГБЦ'))
-            for operation in operations:
-                cost_operation = operation.price * self.count_of_valves
-
-            return cost_operation
-
-    code = models.CharField('Номер заказа',
-                            max_length=255,
-                            db_index=True,
-                            default=generate_code,
-                            )
-    customer_name = models.CharField('Имя заказчика', max_length=50)
-    customer_phone_number = models.CharField('Номер заказчика', max_length=12)
-    engine_title = models.CharField('Наименование двигателя', max_length=50)
-    accept_date = models.DateTimeField('Дата приёма', auto_now=True)
-    count_of_cylinders = models.IntegerField('Количество цилиндров', default=0)
-    count_of_valves = models.IntegerField('Количество клапанов', default=0)
+    id = models.AutoField("Номер заказа", primary_key=True, unique=True)
+    customer_name = models.CharField("Имя заказчика", max_length=50)
+    customer_phone_number = models.CharField("Номер заказчика", max_length=12)
+    engine = models.ForeignKey(Engine,
+                               on_delete=models.CASCADE,
+                               blank=True,
+                               null=True)
     operation = models.ManyToManyField(Operation,
-                                       verbose_name='Операция',
-                                       related_name='task'
+                                       verbose_name="Операция",
+                                       related_name="task"
                                        )
-    total_amount = models.PositiveIntegerField('Итоговая сумма', default=0)
-    comments = models.TextField('Комментарии', blank=True, null=True)
-    complete_date = models.DateTimeField('Дата выполнения',
+    total_amount = models.PositiveIntegerField("Итоговая сумма", default=0)
+    comments = models.TextField("Комментарии", blank=True, null=True)
+    accept_date = models.DateTimeField("Дата приёма", default=timezone.now())
+    complete_date = models.DateTimeField("Дата выполнения",
                                          blank=True,
-                                         null=True)
+                                         null=True
+                                         )
 
     class Meta:
-        ordering = ['-accept_date']
-        verbose_name = 'заказ'
-        verbose_name_plural = 'заказы'
+        ordering = ["-accept_date"]
+        indexes = [
+            models.Index(fields=['id']),
+            models.Index(fields=['customer_name']),
+            models.Index(fields=['engine'])
+        ]
+        verbose_name = "заказ"
+        verbose_name_plural = "заказы"
 
     def __str__(self):
-        return self.code + ' ' + self.customer_name
-
-    def save(self, *args, **kwargs):
-        self.total_amount = self.sum_operation()
-        super().save(*args, **kwargs)
-
+        return f'{self.id} {self.customer_name}'
